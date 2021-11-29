ce0e2e95247d849e43266523f9215642d8d184ce
Rueben <rubzdebruyn@gmail.com>
2021-11-29 15:51:16 +0200

    monday work

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,11 +1,37 @@
 import React from "react";
-import { PinkBg, RecipeH1 } from "./PostsStyled";
+import {
+  PinkBg,
+  Postcontainer,
+  RecipeCard,
+  RecipeH1,
+  RecipeTitle,
+} from "./PostsStyled";
+
+// import all images her(this is hardcoding the srcs)
+import Cake from "../assets/weddingC.png";
 
 const Posts = () => {
   return (
     <>
       <PinkBg>
-        <RecipeH1>Heading 1</RecipeH1>
+        <RecipeH1>Recipes</RecipeH1>
+        <Postcontainer>
+          <RecipeCard background={Cake}>
+            <RecipeTitle>Title</RecipeTitle>
+          </RecipeCard>
+          <RecipeCard background={Cake}>
+            <RecipeTitle>Title</RecipeTitle>
+          </RecipeCard>
+          <RecipeCard background={Cake}>
+            <RecipeTitle>Title</RecipeTitle>
+          </RecipeCard>
+          <RecipeCard background={Cake}>
+            <RecipeTitle>Title</RecipeTitle>
+          </RecipeCard>
+          <RecipeCard background={Cake}>
+            <RecipeTitle>Title</RecipeTitle>
+          </RecipeCard>
+        </Postcontainer>
       </PinkBg>
     </>
   );
--- src/components/Posts/PostsStyled.js
+++ src/components/Posts/PostsStyled.js
@@ -6,7 +6,7 @@ import styled from "styled-components";
 // container for posts//
 export const PinkBg = styled.div`
   width: 100%;
-  height: 65vh;
+  height: fit-content;
   background-color: #f5c3d7;
   margin: 3rem 0px;
 `;
@@ -17,4 +17,44 @@ export const RecipeH1 = styled.h1`
   justify-content: center;
   padding: 0.5em;
 `;
-export const Items = styled.div``;
+
+export const Postcontainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  background-color: #f5c3d7;
+  height: auto;
+  margin: 1rem 0px;
+  gap: 15px 0px;
+`;
+
+export const RecipeCard = styled.div`
+  display: flex;
+  min-height: 15em;
+  min-width: 15rem;
+  flex-wrap: 1;
+  margin: 10px;
+  background-color: honeydew;
+  /* flex: 0 1 200px; */
+  background-image: url(${({ background }) => background});
+  background-size: cover;
+  /* box-shadow: 1px 1px 10px 3px rgb(246, 233, 238, 0.8); */
+`;
+
+export const RecipeTitle = styled.h3`
+  position: relative;
+  margin: 0 auto;
+  top: 83%;
+  background-color: rgb(246, 233, 238, 0.8);
+  height: fit-content;
+  padding: 0.5rem 0px;
+  width: 100%;
+  text-align: center;
+  color: #363737;
+`;
+// export const ItemPic = styled.div`
+//   height: 50%;
+//   background-image: url(${({ background }) => background});
+//   background-repeat: no-repeat;
+//   background-size: cover;
+// `;
